8b449b99a2eb2733d673a46704bb2bee64427914
cmis <cmis@cmis-hs04.(none)>
2017-08-08 11:52:54 +0700

    BLUEJ

---
+++ HelloWorld/WhatareClasses.java
@@ -0,0 +1,12 @@
+
+public class WhatareClasses
+{
+    // instance variables - replace the example below with your own
+    private String Hello = "Hello I am sad"; 
+
+   
+    public WhatareClasses()
+    {
+      System.out.print (Hello);
+    }
+}
