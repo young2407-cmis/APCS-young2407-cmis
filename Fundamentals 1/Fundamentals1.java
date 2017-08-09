a4722f144c46800bfd52291155d2a0b074f6d5d0
cmis <cmis@cmis-hs04.(none)>
2017-08-09 14:35:43 +0700

    I need help

---
+++ Fundamentals 1/Fundamentals1.java
@@ -0,0 +1,16 @@
+public class Fundamentals1
+    {
+    public static void dataTypes (String args [])
+        {
+        byte a= 15;
+        short a= 19124;
+        int a= 1231123123;
+        long a= 123123L;
+        float a= 199.3F;
+        double a= 311.3D;
+        boolean a= true;
+        char A= 'A';
+    }
+    }
+        
+ 
