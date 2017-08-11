3567cdc15aee005a3169a0e57720a702f89a3a07
cmis <cmis@cmis-hs04.(none)>
2017-08-11 09:56:05 +0700

    number 1

--- Fundamentals 1/Fundamentals1.java
+++ Fundamentals 1/Fundamentals1.java
@@ -1,15 +1,35 @@
 public class Fundamentals1
     {
-    public static void dataTypes (String args [])
+    public static void main (String args [])
         {
-        byte a= 15;
-        short a= 19124;
-        int a= 1231123123;
-        long a= 123123L;
-        float a= 199.3F;
-        double a= 311.3D;
-        boolean a= true;
-        char A= 'A';
+        System.out.println( "Hello everyone out there! "); //Display the string
+        
+        } //end method main
+    public static void dataTypes ()
+        {
+        byte byteType= 15;
+        System.out.println("8bit, limited integer datatype. Byte could be used to save memory when it is used to represent someone's age");
+
+        short shortType= 1912;
+        System.out.println("16bit, limited integer datatype that is one step larger than byte. Also used to save memory and could be used to represent the year?");
+        
+        int intType= 123181092;
+        System.out.println("32bit, limited integer datatype. Integer is smaller than long and could be used to save memory. It can be used in more complex methods involving constant addition, multiplication, subtraction, or division.");
+               
+        long longType= 123123L;
+        System.out.println("64bit, integer datatype. It is used to store a larger value integer datatype can cover.");
+        
+        float floatType= 199.3F;
+        System.out.println("32 bit, decimal datatype. It is used to save memory and is not as accurate as other decimal datatypes. It can be used in simple math functions(division with its unique form of floating points) in different methods");
+        
+        double doubleType= 311.3D;
+        System.out.println("64 bit, decimal datatype. Default datatype for decimal values but is not accurate. Could be used in simple calculations");
+        
+        boolean booleanType= true;
+        System.out.println("Boolean has two values: True and False. Could be useful in conditionals and in for loops to read and react.");
+        
+        char charType= 'A';
+        System.out.println("Char datatype is used to store any character. Could be used in methods to create unicodes for a product.");
     }
     }
         
