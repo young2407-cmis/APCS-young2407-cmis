fbff5f5e1a028b23609e2a2f3d4e43ef376387fb
cmis <cmis@cmis-hs06.(none)>
2017-09-15 09:55:15 +0700

    number 5

--- Fundamentals 2/Fundamentals2.java
+++ Fundamentals 2/Fundamentals2.java
@@ -1,6 +1,7 @@
+import java.util.concurrent.ThreadLocalRandom;
 public class Fundamentals2{
     public static void main(){
-        int [] intList= {1,2,3,4,5};
+        int [] intList= {1,2,3,4,5,6};
         double[] doubleList= {1.3,5.2,9.8,0.2,7.4};
         String[] stringList= {"first","second","third","fourth","fifth"};
         boolean[] booleanList= {true, false, true, false, true};
@@ -79,4 +80,22 @@ public class Fundamentals2{
     public static int middleItem(int[] array){
         return (array[(int)(array.length/2)]);
     }
-} //end class Fundamentals2
+    public static double middleItem(double[] array){
+        return (array[(int)(array.length/2)]);
+    }
+    public static String middleItem(String[] array){
+        return (array[(int)(array.length/2)]);
+    }
+    public static boolean middleItem(boolean[] array){
+        return (array[(int)(array.length/2)]);
+    }
+    public static int[] randomInts(int n, int min, int max){
+        int[] output;
+        while (n--){
+            int random= ThreadLocalRandom.current().nextInt(min, max + 1);
+            int[] output + random;
+        }
+        return output;
+    }
+
+//} //end class Fundamentals2
