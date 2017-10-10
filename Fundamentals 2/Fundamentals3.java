5475d426eeda290751f2e8bfaf0ef53e0b3288e8
cmis <cmis@cmis-hs06.(none)>
2017-10-10 09:55:23 +0700

    asdasdasd

--- Fundamentals 2/Fundamentals3.java
+++ Fundamentals 2/Fundamentals3.java
@@ -16,17 +16,22 @@ public class Fundamentals3{
                 {" ","#"," ","#"," ","#"," ","#"},
                 {"#"," ","#"," ","#"," ","#"," "},
                 {" ","#"," ","#"," ","#"," ","#"}};
-        initializeArray(literalArray1, 9);
+        snakePrint(literalArray1);
+
     }
+
     public static void setValue(int[][] arr, int r, int c, int v){
         arr[r][c]=v;
     }
+
     public static void setValue(double[][] arr, int r, int c, double v){
         arr[r][c]=v;
     }
+
     public static void setValue(String[][] arr, int r, int c, String v){
         arr[r][c]=v;
     }
+
     public static void initializeArray(int[][] array, int value){
         for(int y=0;y<array.length;y++){
             for(int x=0;x<array[y].length;x++){
@@ -34,5 +39,64 @@ public class Fundamentals3{
             }
         }
     }
-    
-}
+
+    public static void initializeArray(double[][] array, double value){
+        for(int y=0;y<array.length;y++){
+            for(int x=0;x<array[y].length;x++){
+                array[y][x]=value;
+            }
+        }
+    }
+
+    public static void initializeArray(boolean[][] array, boolean value){
+        for(int y=0;y<array.length;y++){
+            for(int x=0;x<array[y].length;x++){
+                array[y][x]=value;
+            }
+        }
+    }
+
+    public static void initializeArray(String[][] array, String value){
+        for(int y=0;y<array.length;y++){
+            for(int x=0;x<array[y].length;x++){
+                array[y][x]=value;
+            }
+        }
+    }
+
+    public static void print2DArray(int[][] array, boolean rowMajor){
+        if(rowMajor==true){
+            for(int y=0; y<array.length; y++){
+                for(int x=0; x<array.length; x++){
+                    System.out.print(array[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+        else{
+            for(int x=0; x<array.length; x++){
+                for(int y=0; y<array.length; y++){
+                    System.out.print(array[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static void snakePrint(int[][] arr){
+        for(int y=0; y<arr.length; y++){
+            if(y%2!=0){
+                for(int x=0; x<arr[y].length; x++){
+                    System.out.print(arr[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+            else if(y%2==0){
+                for(int x=arr[y].length; x>0; x--){
+                    System.out.print(arr[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+} 
