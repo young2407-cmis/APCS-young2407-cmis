8faa16157e366d39d4464120c67d1020b58d67c8
aa <aa>
2018-01-15 11:56:09 +0700

    asd

--- MergeSort/Merge.java
+++ MergeSort/Merge.java
@@ -1,43 +1,18 @@
 public class Merge
 {
     public static void main(){
-        int[] array1= {5,4,3,2,1,6,7,8,9,10};
+        int[] array1= {2,4,3,2,1,6,7,8,9,10};
         int[] array2= {1,2,3,4,5,6,7,8,9,10};
-        int[][] array3= {{1,2,3,4,5},{1,2,3,4,5}};
-
-        printArray(firstHalf(array1));
-        printArray(secondHalf(array1));
-        printArray(delete(array1, 0));
+        int[] array3= {1,2,3,4,5,6,7};
+        //printArray(firstHalf(array1));
+        //printArray(secondHalf(array1));
+        delete(array3,0);
+        printArray(array3);
+        
         //printArray(merge(array1, array2));
-        print2DArray(split2(array1),true);
-        print2DArray(array3, true);
+        //print2DArray(split(array1),true);
+        printArray(merge(array1, array2));
     }
-
-    public static void printArray(int[] array){
-        for(int i : array){
-            System.out.print(i+ " ");
-        }
-        System.out.print("\n");
-    }
-
-     public static void print2DArray(int[][] array, boolean rowMajor){
-        if(rowMajor==true){
-            for(int y=0; y<array.length; y++){
-                for(int x=0; x<array.length; x++){
-                    System.out.print(array[y][x]+" ");
-                }
-                System.out.print("\n");
-            }
-        }
-        else{
-            for(int x=0; x<array.length; x++){
-                for(int y=0; y<array.length; y++){
-                    System.out.print(array[y][x]+" ");
-                }
-                System.out.print("\n");
-            }
-        }
-    } //end print2DArray
     
     public static int[] firstHalf(int[] array){
         int[] returnArray= new int [array.length/2];
@@ -54,20 +29,8 @@ public class Merge
         }    
         return returnArray;
     }
-
-    public static int[][] split(int[] array){
-        int biggerHalfLength= array.length-(array.length/2);
-        int[][] return2DArray= new int [2][biggerHalfLength];
-        for(int i=0; i<biggerHalfLength; i++){
-            return2DArray[0][i] = array[i];
-        }
-        for(int i2=biggerHalfLength; i2<array.length; i2++){
-            return2DArray[1][i2-biggerHalfLength] = array[i2];
-        }
-        return return2DArray;
-    }
     
-    public static int[][] split2(int[]array){
+    public static int[][] split(int[]array){
         int[][] return2DArray= {firstHalf(array),secondHalf(array)};
         return return2DArray;
     }
@@ -88,13 +51,21 @@ public class Merge
         }
         return returnArray;
     }
+    
+    public static int[] merge2(int[] arraya, int[] arrayb){
+        int[] returnArray= new int [arraya.length+arrayb.length];
+        if(arraya.length + arrayb.length == 0){
+            return
+        }
+    }
+    
 
-    public static int[] delete(int[] array, int i){
+    public static void delete(int[] array, int i){
         int[] returnArray= new int [array.length-1];
         for(int k=i;k<array.length-1;k++){
             returnArray[k]=array[k+1];
         }
-        return returnArray;
+        array= returnArray;
     }
 
     public static int[] mergeSort(int[] array){
@@ -109,4 +80,30 @@ public class Merge
         }
         return merge(array1, array2);
     }
+    
+    public static void printArray(int[] array){
+        for(int i : array){
+            System.out.print(i+ " ");
+        }
+        System.out.print("\n");
+    }
+
+     public static void print2DArray(int[][] array, boolean rowMajor){
+        if(rowMajor==true){
+            for(int y=0; y<array.length; y++){
+                for(int x=0; x<array[y].length; x++){
+                    System.out.print(array[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+        else{
+            for(int x=0; x<array.length; x++){
+                for(int y=0; y<array[y].length; y++){
+                    System.out.print(array[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+    } //end print2DArray
 }
---
+++ MergeSort/MergeClass2nd.java
@@ -0,0 +1,83 @@
+
+public class MergeClass2nd
+{
+    public static void main(){
+        int[] array1= {2,4,3,2,1,6,7,8,9,10};
+        int[] array2= {1,2,3,4,5,6,7,8,9,10};
+        int[] array3= {1,2,3,4,5,6,7};
+        //printArray(firstHalf(array1));
+        //printArray(secondHalf(array1));        
+        //printArray(merge(array1, array2));
+        //print2DArray(split(array1),true);
+        //printArray(merge(array1, array2));
+        printArray(merge(array1, array2));
+    }
+    
+    public static int[] mergeSort(int[] array){
+        return array;
+    }
+    
+    public static int[] merge(int[] arraya, int[] arrayb){
+        int[] returnArray= new int [arraya.length+ arrayb.length];
+        int a= 0;
+        int b= 0;
+        for(int i=0;i<returnArray.length;i++){
+            if(arraya[a]>arrayb[b]){
+                returnArray[i]= arraya[a];
+                a++;
+            }
+            else{
+                returnArray[i]= arrayb[b];
+                b++;
+            }
+        }
+        return returnArray;
+    }
+    
+    public static int[][] split(int[]array){
+        int[][] return2DArray= {firstHalf(array),secondHalf(array)};
+        return return2DArray;
+    }
+    
+    public static int[] firstHalf(int[] array){
+        int[] returnArray= new int [array.length/2];
+        for(int i=0; i<returnArray.length; i++){
+            returnArray[i] = array[i];
+        }
+        return returnArray;
+    }
+
+    public static int[] secondHalf(int[] array){
+        int[] returnArray= new int [array.length - (array.length/2)];
+        for(int i=returnArray.length; i<array.length; i++){
+            returnArray[i-returnArray.length] = array[i];
+        }    
+        return returnArray;
+    }
+    
+    public static void printArray(int[] array){
+        for(int i : array){
+            System.out.print(i+ " ");
+        }
+        System.out.print("\n");
+    }
+
+     public static void print2DArray(int[][] array, boolean rowMajor){
+        if(rowMajor==true){
+            for(int y=0; y<array.length; y++){
+                for(int x=0; x<array[y].length; x++){
+                    System.out.print(array[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+        else{
+            for(int x=0; x<array.length; x++){
+                for(int y=0; y<array[y].length; y++){
+                    System.out.print(array[y][x]+" ");
+                }
+                System.out.print("\n");
+            }
+        }
+    } //end print2DArray
+}
