5f829a6e8ec3d8dded02615d73cefc3ac51820ae
asd <lalaland>
2018-01-22 11:55:23 +0700

    asd

---
+++ OOP/Program.java
@@ -0,0 +1,12 @@
+public class Program{
+    public static void main(String[] args){
+        Snorlax s1 = new Snorlax();
+        s1.name = "fato";
+        System.out.println(s1);
+        System.out.println(s1.gender);
+        System.out.println(s1.name);
+        System.out.println(s1.level);
+        System.out.println(s1.weight);
+        System.out.println(s1.height);
+    }
+}
