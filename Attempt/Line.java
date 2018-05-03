340dbd4f22dd3577a9bf78ded8b625a437242053
asd <asd>
2018-05-03 14:35:48 +0700

    asd

---
+++ Attempt/Line.java
@@ -0,0 +1,9 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Line extends Actor
+{
+    public void act() 
+    {
+       if(greenfoot.iskey
+    }    
+}
